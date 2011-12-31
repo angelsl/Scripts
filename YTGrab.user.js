ecd5453b426aab139cdc6b0e767777de37efd60e
angelsl <hidingfromhidden@gmail.com>
2012-01-01 01:26:50 +0800

    Yay! Greasemonkey!

---
+++ YTGrab.user.js
@@ -0,0 +1,86 @@
+// ==UserScript==
+// @name          YouTube Download Button
+// @namespace     http://angelsl.org/
+// @description	  Inserts a download button on youtube watch pages
+// @include       http://www.youtube.com/watch*
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
+// ==/UserScript==
+
+var title = $('meta[name=title]').attr("content");
+var map = sortMap(getUrlMap());
+var ul = $("<ul style=\"list-style: disc inside none !important;\" />");
+for(var q in map) {
+    var li = $("<li />").append("<strong>"+q+"</strong> &mdash;");
+    for(var idx = 0; idx < map[q].length; idx++) {
+        li.append(" ").append(map[q][idx]);
+    }
+    ul.append(li);
+}
+var container = $("<div id=\"angelsl-download-area-container\" style=\"height: auto; margin-bottom: 10px; display: none;\" />").append($("<div class=\"yt-rounded\" id=\"angelsl-download-area\" style=\"background: none repeat scroll 0 0 #FFFFFF; border: 0 none; border-radius: 3px 3px 3px 3px; padding:10px; box-shadow: 0 1px 1px #CCCCCC;\" />").append("<strong>Download</strong>").append(ul)).insertBefore($("#watch-info"));
+$("#watch-share").after($("<button data-tooltip-text=\"Download this video\" onclick=\";return false;\" title=\"Download this video\" type=\"button\" class=\"yt-uix-tooltip-reverse yt-uix-button yt-uix-tooltip\" id=\"watch-angelsl-download\" role=\"button\"><span class=\"yt-uix-button-content\">Download</span></button>")
+    .click( function(eo)
+            {
+                eo.preventDefault();
+                container.slideToggle("fast");
+            }));
+
+function getUrlMap() {
+    var regx = /url_encoded_fmt_stream_map=(.+?)(?:&|$)/i.exec($("#movie_player").attr("flashvars"));
+    if(!regx) return false;
+    regx = decodeURIComponent(regx[1]).split(",");
+    var e = [];
+    for(var idx = 0; idx < regx.length; idx++) {
+        e.push(stringToDict(regx[idx]));
+    }
+    return e;
+}
+
+function sortMap(map) {
+    var ret = {};
+    for(var idx = 0; idx < map.length; idx++)
+    {
+        var q = getQuality(map[idx]["quality"]);
+        if(!(q in ret)) ret[q] = [];
+        ret[q].push(getLink(map[idx]));
+    }
+    return ret;
+}
+
+function stringToDict(entry) {
+    var ret = {};
+    var a = entry.split("&");
+    for(var idx = 0; idx < a.length; idx++) {
+        var c = a[idx].split("=");
+        ret[c[0]] = decodeURIComponent(c[1]);
+    }
+    return ret;
+}
+
+function getLink(entry) {
+    var url = entry["url"] + "&title=" + encodeURIComponent(title);
+    var tooltip = "Format ID: " + entry["itag"] + " | Quality: " + entry["quality"] + " | Mime: " + entry["type"];
+    var desc = getType(entry["type"]).toUpperCase();
+    return $("<a href=\"" + url + "\" title=\"" + tooltip + "\">" + desc + "</a>");
+}
+
+function getType(etype) {
+    var regx = /^[-\w+]+\/(?:x-)?([-\w+]+)/.exec(etype);
+    if(!regx) return "MISSINGNO.";
+    return regx[1];
+}
+
+function getQuality(equality) {
+    switch(equality.toLowerCase()) {
+        case "hd1080":
+            return "1080p";
+        case "hd720":
+            return "720p";
+        case "large":
+            return "480p";
+        case "medium":
+            return "360p";
+        case "small":
+            return "240p";
+    }
+    return equality.toLowerCase();
+}
---
+++ tpw.wavtoaudio.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name          theportalwiki.com voice lines embed
+// @namespace     http://angelsl.org/
+// @description	  Inserts a HTML5 <audio> tag into the page if a WAV link is clicked on TPW's Voice lines
+// @include       http://theportalwiki.com/wiki/*_voice_lines*
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
+// @require       http://jquery.malsup.com/media/jquery.media.js?v.92
+// ==/UserScript==
+
+var testRegx = /.*wav$/i;
+var currentElem = null;
+$.fn.media.mapFormat('wav', 'winmedia');
+$("a").filter(function() { return testRegx.test($(this).attr('href')); }).click(function(e) 
+    {
+        e.preventDefault(); if(this == currentElem) return; currentElem = this;
+        $("#sltpwvle117").slideUp('fast', function() { $(this).remove(); });
+        $("#sltpwvle343").removeAttr("id").slideDown('fast');
+        var c = $(this);
+        var y = c.clone();
+        c.attr("id", "sltpwvle343").slideUp('fast');
+        var x = $("<div id=\"sltpwvle117\" style=\"display:none;\"></div>").append(y);
+        y.media({ width: "100%", height: 70, autoplay: true, params: {volume:"100"} }); 
+        c.after(x); x.slideDown('fast');
+    });
