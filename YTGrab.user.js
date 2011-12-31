036246e6b5e8cf6e86ee5fad33e9f3cac204facb
angelsl <hidingfromhidden@gmail.com>
2012-01-01 01:42:57 +0800

    update LGPL/namespace

--- YTGrab.user.js
+++ YTGrab.user.js
@@ -1,10 +1,25 @@
 // ==UserScript==
 // @name          YouTube Download Button
-// @namespace     http://angelsl.org/
-// @description	  Inserts a download button on youtube watch pages
+// @namespace     https://github.com/angelsl/misc-Greasemonkey
+// @description	  Inserts a download button on YouTube video pages
+// @version       1.0
 // @include       http://www.youtube.com/watch*
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
 // ==/UserScript==
+/* 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 var title = $('meta[name=title]').attr("content");
 var map = sortMap(getUrlMap());
--- tpw.wavtoaudio.user.js
+++ tpw.wavtoaudio.user.js
@@ -1,11 +1,25 @@
 // ==UserScript==
 // @name          theportalwiki.com voice lines embed
-// @namespace     http://angelsl.org/
+// @namespace     https://github.com/angelsl/misc-Greasemonkey
 // @description	  Inserts a HTML5 <audio> tag into the page if a WAV link is clicked on TPW's Voice lines
 // @include       http://theportalwiki.com/wiki/*_voice_lines*
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
 // @require       http://jquery.malsup.com/media/jquery.media.js?v.92
 // ==/UserScript==
+/* 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 var testRegx = /.*wav$/i;
 var currentElem = null;
