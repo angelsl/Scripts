fc09b5653b0fe3981a8d8037a6fc5549eac74710
angelsl <hidingfromhidden@gmail.com>
2012-01-10 11:50:08 +0800

    Credits.

--- YTGrab.user.js
+++ YTGrab.user.js
@@ -7,6 +7,9 @@
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
 // ==/UserScript==
 /* 
+	Originally written by angelsl
+	With contributions from Manish Burman http://mburman.com
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
