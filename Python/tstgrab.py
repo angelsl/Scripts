3c4769164507b2d3b2aaaaf573c88c89fa22f6b9
angelsl <hidingfromhidden@gmail.com>
2012-06-25 17:00:18 +0800

    tstgrab.py: Update output formatting.
    Signed-off-by: angelsl <hidingfromhidden@gmail.com>

--- Python/tstgrab.py
+++ Python/tstgrab.py
@@ -11,34 +11,40 @@ from pyPdf import PdfFileWriter, PdfFileReader
 LAYOUT_URL = "http://st.buuuk.in/ipad_layout"
 
 def main():
-    print "Getting issue data."
+    print "Getting issue data from", LAYOUT_URL
     sections = json.loads(getfromurl(LAYOUT_URL))['sections']
     filenames = []
     for section in sections:
         dosection(section, filenames)
     toclose = []
     out = PdfFileWriter()
+    print 'Retrieving pages'
     for file in filenames:
         f = open(file, 'rb')
         toclose.append(f)
-        print 'Adding pages from', file
+        print '--> Adding page(s) from', file
         fpdf = PdfFileReader(f)
         for pdfpg in fpdf.pages:
             out.addPage(pdfpg)
-    of = open(''.join(['ST', date.today().strftime('%Y%m%d'), '.pdf']), 'wb')
+    ofname = ''.join(['ST', date.today().strftime('%Y%m%d'), '.pdf'])
+    of = open(ofname, 'wb')
     toclose.append(of)
+    print 'Merging pages'
     out.write(of)
     for f in toclose:
         f.close()
+    print 'Deleting temporary files'
     for file in filenames:
+        print '--> Deleting', file
         os.unlink(file)
+    print 'Done!', ofname
 
 def dosection(section, filenames):
-    print ''.join(["Processing section ", section['category']])
+    print "Processing section", section['category']
     pdfbase = section['pdf_url']
     for page in section['pdf_pages']:
         url = ''.join([pdfbase, page])
-        print ''.join(["--> ", "Downloading ", url])
+        print "--> Downloading", url
         name = posixpath.basename(urlparse.urlsplit(url)[2])
         with open(name, 'wb') as f:
             f.write(getfromurl(url))
