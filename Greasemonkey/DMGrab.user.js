99c7e5c93dcb07c710a7d07b84ce0004c0758bac
angelsl <hidingfromhidden@gmail.com>
2012-12-09 19:49:16 +0800

    Scripts: DMGrab: Update preamble
    Signed-off-by: angelsl <hidingfromhidden@gmail.com>

--- Greasemonkey/DMGrab.user.js
+++ Greasemonkey/DMGrab.user.js
@@ -1,19 +1,9 @@
-/** Copyright (C) 2012 angelsl
-    
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+/** Originally written by angelsl
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    DMGrab is distributed under the GNU LGPL v3 or later and comes with no warranty.
+    Full preamble at https://github.com/angelsl/misc-Scripts/blob/master/Greasemonkey/LICENSE.md#lgpl
     
-    jQuery v1.8.3 -deprecated,-css,-ajax,-ajax/jsonp,-ajax/script,-ajax/xhr,-effects,-offset,-dimensions jquery.com | jquery.org/license
+    jQuery v1.8.3 -deprecated,-ajax,-ajax/jsonp,-ajax/script,-ajax/xhr,-effects,-offset,-dimensions jquery.com | jquery.org/license
     
 // ==UserScript==
 // @name          Dailymotion Download Button
