51a9dcd27c01cea9b9b94f4b53557d0e365afc84
angelsl <hidingfromhidden@gmail.com>
2013-07-08 19:20:33 +0800

    Scripts: tstgrab: Some improvements.
    Signed-off-by: angelsl <hidingfromhidden@gmail.com>

--- Python/tstgrab.py
+++ Python/tstgrab.py
@@ -8,57 +8,60 @@ import os
 import sys
 from datetime import date, timedelta
 from pyPdf import PdfFileWriter, PdfFileReader
+import threadpool
 
-LAYOUT_URL = "http://st.buuuk.in/ipad_layout?day="
+LAYOUT_URL_ = "http://st.buuuk.in/ipad_layout?day="
+verbose = False
 
-def main():
-    global LAYOUT_URL
-    daysago = 0
-    if len(sys.argv) > 1:
-        daysago = sys.argv[1]
-    LAYOUT_URL += str(daysago)
-    print "Getting issue data from", LAYOUT_URL
+def doday(daysago):
+    global LAYOUT_URL_
+    LAYOUT_URL = LAYOUT_URL_ + str(daysago)
+    if verbose: print "Getting issue data from", LAYOUT_URL
     sections = json.loads(getfromurl(LAYOUT_URL))['sections']
     filenames = []
     for section in sections:
         dosection(section, filenames)
     toclose = []
     out = PdfFileWriter()
-    print 'Retrieving pages'
+    if verbose: print 'Retrieving pages'
     for file in filenames:
         f = open(file, 'rb')
         toclose.append(f)
-        print '--> Adding page(s) from', file
+        if verbose: print '--> Adding page(s) from', file
         fpdf = PdfFileReader(f)
         for pdfpg in fpdf.pages:
             out.addPage(pdfpg)
     ofname = ''.join(['ST', (date.today() - timedelta(int(daysago))).strftime('%Y%m%d'), '.pdf'])
     of = open(ofname, 'wb')
     toclose.append(of)
-    print 'Merging pages'
+    if verbose: print 'Merging pages'
     out.write(of)
     for f in toclose:
         f.close()
-    print 'Deleting temporary files'
+    if verbose: print 'Deleting temporary files'
     for file in filenames:
-        print '--> Deleting', file
+        if verbose: print '--> Deleting', file
         os.unlink(file)
-    print 'Done!', ofname
+    print 'Done:', ofname
 
 def dosection(section, filenames):
-    print "Processing section", section['category']
+    if verbose: print "Processing section", section['category']
     pdfbase = section['pdf_url']
     for page in section['pdf_pages']:
         url = ''.join([pdfbase, page])
-        print "--> Downloading", url
+        if verbose: print "--> Downloading", url
         name = posixpath.basename(urlparse.urlsplit(url)[2])
-        with open(name, 'wb') as f:
-            f.write(getfromurl(url))
-            f.flush()
+#        with open(name, 'wb') as f:
+            #f.write(getfromurl(url))
+            #f.flush()
         filenames.append(name)
 
 def getfromurl(url):
     return urllib2.urlopen(url).read()
 
 if __name__ == '__main__':
-    main()
+    pool = threadpool.ThreadPool(4)
+    req = threadpool.makeRequests(doday, xrange(int(sys.argv[1]), int(sys.argv[2])))
+    for reqs in req:
+        pool.putRequest(reqs)
+    pool.wait()
