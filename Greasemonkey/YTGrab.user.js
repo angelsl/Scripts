0a531be897effb45926699a12995dce52f2e29ae
angelsl <hidingfromhidden@gmail.com>
2014-08-31 16:19:19 +0800

    YTGrab: Push version to 1.75.3
    Signed-off-by: angelsl <hidingfromhidden@gmail.com>

--- Greasemonkey/YTGrab.user.js
+++ Greasemonkey/YTGrab.user.js
@@ -9,7 +9,7 @@
 // @name          YouTube Download Button
 // @namespace     https://github.com/angelsl/misc-Scripts
 // @description   Inserts a download button on YouTube video pages
-// @version       1.75.2
+// @version       1.75.3
 // @run-at        document-end
 // @updateURL     https://github.com/angelsl/misc-Scripts/raw/master/Greasemonkey/YTGrab.user.js
 // @downloadURL   https://github.com/angelsl/misc-Scripts/raw/master/Greasemonkey/YTGrab.user.js
