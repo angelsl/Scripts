d3a430f5358a285ecd05a49e14295a79ac6b6999
fantasyspace <sweetenl.acie@gmail.com>
2017-10-30 18:35:53 +0800

    YTGrab: Bump version

--- Greasemonkey/YTGrab.user.js
+++ Greasemonkey/YTGrab.user.js
@@ -9,7 +9,7 @@
 // @name          YouTube Download Button
 // @namespace     https://github.com/angelsl/misc-Scripts
 // @description   Inserts a download button on YouTube video pages
-// @version       2.03
+// @version       2.031
 // @run-at        document-start
 // @updateURL     https://github.com/angelsl/misc-Scripts/raw/master/Greasemonkey/YTGrab.user.js
 // @downloadURL   https://github.com/angelsl/misc-Scripts/raw/master/Greasemonkey/YTGrab.user.js
